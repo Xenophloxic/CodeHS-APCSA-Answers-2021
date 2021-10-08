168979d99ec3d5359d66719957ac17e4bdd9d6b8
Xenophloxic <64376702+Xenophloxic@users.noreply.github.com>
2021-10-08 15:01:08 -0700

    not done yet

---
+++ Unit 2 - Primative Types/1.2/1.2.5 Our First Integer.java
@@ -0,0 +1,8 @@
+public class Variables
+{
+    public static void main(String[] args)
+    {
+        int year = 2021; // declaring an integar variable and setting it to "2021" DON'T FORGET TO CHANGE THIS TO THE ACTUAL YEAR
+        System.out.println("The current year is " + year); // printing out the year along with some text
+    }
+}
---
+++ Unit 2 - Primative Types/1.2/1.2.9 Answering Questions.java
@@ -0,0 +1,9 @@
+public class Variables
+{
+    public static void main(String[] args)
+    {
+        String myName = "Karel the Dog"; // declaring a string variable and giving it the value of "Karel the Dog"
+        int luckyNumber = 11; // declaring an integar with the value "11"
+        double currentTemperature = 
+    }
+}
