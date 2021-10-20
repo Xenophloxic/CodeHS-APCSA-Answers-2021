83241d55e04cab471d605185bebdf3da6a2803b1
Xenophloxic <64376702+Xenophloxic@users.noreply.github.com>
2021-10-19 18:05:50 -0700

    catching some up

--- Unit 1 - Primative Types/1.2 Variables and Data Types/1.2.9 Answering Questions.java
+++ Unit 1 - Primative Types/1.2 Variables and Data Types/1.2.9 Answering Questions.java
@@ -4,6 +4,13 @@ public class Variables
     {
         String myName = "Karel the Dog"; // declaring a string variable and giving it the value of "Karel the Dog"
         int luckyNumber = 11; // declaring an integar with the value "11"
-        double currentTemperature = 
+        double currentTemperature = 75.3; // declaring the double currentTemperature with the value "75.3"
+        boolean isStudent = true; // declaring a boolean with the value "true"
+
+        // print out the info
+        System.out.println(myName);
+        System.out.println(luckyNumber);
+        System.out.println(currentTemperature);
+        System.out.println(isStudent);
     }
 }
