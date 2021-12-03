5b5d69b2d29c68520178078a1eecf4e1902373c1
Xenophloxic <64376702+Xenophloxic@users.noreply.github.com>
2021-12-03 09:13:55 -0800

    5.1

---
+++ Unit 5 - Writing Classes/5.1 Writing Classes/5.1.5 Access for Employee class.java
@@ -0,0 +1,10 @@
+public class Employee
+{
+    private String name = "";
+    private int ID = 0;
+    private double hourlySalary = 0.0;
+    
+    public Employee(String name, int ID, double salary){
+        
+    }
+}
